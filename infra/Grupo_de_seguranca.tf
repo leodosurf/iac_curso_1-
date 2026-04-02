0a2889b84a74bb0bc0e5b3b3c7544134b3e69754
leodosurf <leonardo.prado.mcse@gmail.com>
2026-04-02 11:45:43 -0300

    aula26-3

--- infra/Grupo_de_seguranca.tf
+++ infra/Grupo_de_seguranca.tf
@@ -1,5 +1,5 @@
 resource "aws_security_group" "acesso_geral" {
-    name = "acesso_geral"
+    name = "acesso_geral_prod"
     description = "acesso geral"
     ingress{
         cidr_blocks = ["191.181.58.215/32"] # IP da Claro no dia desse treinamento
